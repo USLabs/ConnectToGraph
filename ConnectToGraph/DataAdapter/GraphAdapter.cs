70e6df631150766a967a15353b393e682cc2de22
USLabs <umang.elixir@gmail.com>
2018-05-02 16:21:22 -0700

    ConnectGraph

---
+++ ConnectToGraph/DataAdapter/GraphAdapter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectToGraph.DataAdapter
+{
+    using Neo4j.Driver.V1;
+
+    public class GraphAdapter : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public GraphAdapter(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public String fire(string message)
+        {
+            var session = _driver.Session();
+            var result = session.Run(message);
+            String resultStr = "";
+            foreach (
+                var record in result)
+            {
+                var node = record["n"].As<INode>();
+                foreach (var p in node.Properties)
+                {
+                    resultStr += p.Key + "\t" + p.Value + "\n";
+                }
+            }
+
+            return resultStr;
+            /*
+            var greeting = session.WriteTransaction(tx =>
+            {
+                var result = tx.Run("CREATE (a:Greeting) " +
+                                    "SET a.message = $message " +
+                                    "RETURN a.message + ', from node ' + id(a)",
+                    new { message });
+                return result.Single()[0].As<string>();
+            });
+            */
+            Console.WriteLine("Retrieved");
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
+
+//https://stackoverflow.com/questions/36692425/how-can-i-access-the-data-in-the-result-of-a-call-to-a-neo4j-database-using-the?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
---
+++ ConnectToGraph/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConnectToGraph.DataAdapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectToGraph
+{
+    class Program
+    {
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(getEpochDate("04/14/2018"));
+            //Console.WriteLine(getStringDate(getEpochDate("04/14/2018")));
+            //Console.ReadLine();
+            GraphAdapter adapter = new GraphAdapter("bolt://54.191.234.201:7687", "neo4j", "umang");
+            String query = "match(n) where n.location=~'(?i).*sjsu main campus.*' return (n)";
+            Console.WriteLine(adapter.fire(query));
+        }
+
+        //Enter data in mm/dd/yyyy format
+        public static Int64 getEpochDate(String iDate)
+        {
+            //string iDate = "05/14/2005";
+            DateTime date = Convert.ToDateTime(iDate);
+            return Convert.ToInt64((date.ToUniversalTime() - epoch).TotalSeconds);
+        }
+
+        public static string getStringDate(Int64 epochTime)
+        {
+            return epoch.AddSeconds(epochTime).ToString("MM/dd/yyyy");
+        }
+
+        //Query
+        /*
+            MATCH (node:event)  
+            WHERE node.startDate = '' 
+            RETURN emp
+
+            MATCH (node:event)  
+            WHERE node.startDate < '' AND node.startDate > '' 
+            RETURN emp
+
+            MATCH (node:event)  
+            WHERE node.eventName = '' 
+            RETURN emp
+
+        */
+    }
+}
